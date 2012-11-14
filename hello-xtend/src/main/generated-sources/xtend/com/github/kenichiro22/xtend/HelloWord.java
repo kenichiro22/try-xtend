163353807ccda30e6c4b5b796928538d2d3f1906
Kenichiro TANAKA <tanaka.kenichiro@gmail.com>
2012-11-14 14:52:18 +0900

    Modify xtend generated source folder

---
+++ hello-xtend/src/main/generated-sources/xtend/com/github/kenichiro22/xtend/HelloWord.java
@@ -0,0 +1,10 @@
+package com.github.kenichiro22.xtend;
+
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+@SuppressWarnings("all")
+public class HelloWord {
+  public static void main(final String[] args) {
+    InputOutput.<String>println("Hello world");
+  }
+}
